82f4ecb8bc71a8e51147dbeeb4322e5c4a89395b
persikboisky <166322618+persikboisky@users.noreply.github.com>
2025-05-11 12:13:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,12 @@
 using namespace core;
 using namespace core::math;
 
-std::vector<float> vertexes{
-	-0.05, 0.05, 1, 0,
-	-0.05,-0.05, 1, 1,
-	 0.05,-0.05, 0, 1,
-	 0.05, 0.05, 0, 0
-};
-
-std::vector<unsigned int> indexes{0, 1, 2, 3};
-
-
 int main()
 {
 	try
 	{
 		Core::Init();
-
-		Window window = Window(1280, 720, "test");
-		window.setMaxFPS(75);
-		window.setContext();
-
-		fbo::DepthTest(true);
-		fbo::setColor(RGBA(0, 0, 0, 255));
-
-		Turtle turtle = Turtle(window);
-
-		double time = 0;
-
-		while (!window.event->close())
-		{
-			window.event->update();
-
-			turtle.enableDrawBody(false);
-			if (Core::GetTime() >= time + 0.01)
-			{
-				time = Core::GetTime();
-				turtle.setColor(RGB(255, 0, 255));
-				turtle.setWidth(10);
-				turtle.move();
-				turtle.setAngle(Core::GetTime() * 20);
-			}
-
-			turtle.draw();
-
-			window.swapBuffers();
-			fbo::clearBuffers(COLOR_BUFFER, DEPTH_BUFFER);
-		}
-
-		turtle.clear();
+		// ваш код
 	}
 	catch (...)
 	{
